5b1448730efd61d114eebf907a0f7032a376efe1
Szikszai László <sziklaszlo@gmail.com>
2021-03-05 19:41:57 +0100

    #2 Added additional arguments (#4)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.1.0
+
+Added additional arguments
+
 ## 1.0.0
 
 Added beta code of the tool to support Linux and Windows OS.
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -33,6 +33,19 @@ describe("run", () => {
         expect(() => run()).toThrow("Command is not correct!");
     });
 
+    test("should fail if only arguments set", () => {
+        process.argv = ["node", "bin/with-aws-creds", "--p1=v1"];
+        expect(() => run()).toThrow("There is no command specified!");
+
+        process.argv = ["node", "bin/with-aws-creds", "--p1", "v1"];
+        expect(() => run()).toThrow("There is no command specified!");
+    });
+
+    test("should fail if an argument is set without value", () => {
+        process.argv = ["node", "bin/with-aws-creds", "--p1", "--p2", "v2", "which", "node"];
+        expect(() => run()).toThrow("Argument is not valid, missing value: p1!");
+    });
+
     test("should execute command with credentails", () => {
         (platform as unknown as jest.Mock).mockReturnValue("win32");
         (existsSync as unknown as jest.Mock).mockReturnValue(true);
@@ -55,6 +68,36 @@ describe("run", () => {
                 HOMEPATH: "/homepath",
                 "KEY1": "value1",
                 "KEY2": "value2"
+            },
+            cwd: process.cwd(),
+            stdio: 'inherit',
+            encoding: "utf-8"
+        });
+    });
+
+    test("should execute command with credentails and additional parameters", () => {
+        (platform as unknown as jest.Mock).mockReturnValue("win32");
+        (existsSync as unknown as jest.Mock).mockReturnValue(true);
+        (readFileSync as unknown as jest.Mock).mockReturnValue(`
+        [default]
+        key1 = value1
+        key2=value2
+
+        [profile]
+        key3 =value3  
+        key4= value4
+        `);
+
+        process.env = { HOMEPATH: "/homepath" };
+        process.argv = ["node", "bin/with-aws-creds", "--aws_account_id", "123", "which", "node", "--smth"];
+        run();
+
+        expect(execSync).toHaveBeenCalledWith("which node --smth", {
+            env: {
+                HOMEPATH: "/homepath",
+                "KEY1": "value1",
+                "KEY2": "value2",
+                "AWS_ACCOUNT_ID": "123",
             },
             cwd: process.cwd(),
             stdio: 'inherit',
